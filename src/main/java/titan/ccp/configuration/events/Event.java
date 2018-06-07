0b32a88f2992b786d8d9c1460e4a03950a21a5ce
Sören Henning <post@soeren-henning.de>
2018-06-07 14:18:32 +0200

    moved configuration.events package to titanccp-model project

--- src/main/java/titan/ccp/configuration/events/Event.java
+++
@@ -1,7 +0,0 @@
-package titan.ccp.configuration.events;
-
-public enum Event {
-
-	SENSOR_REGISTRY_CHANGED;
-
-}
--- src/main/java/titan/ccp/configuration/events/EventPublisher.java
+++
@@ -1,9 +0,0 @@
-package titan.ccp.configuration.events;
-
-public interface EventPublisher {
-
-	public void publish(final Event event, final String value);
-
-	public void close();
-
-}
